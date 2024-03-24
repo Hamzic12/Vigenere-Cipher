3c57bc67c018888ea48f48a41deb08c8739e7fb9
unknown <hamza23@seznam.cz>
2024-03-24 15:06:38 +0100

    Added Decryption and choosing

--- Vigenere Cipher.py
+++ Vigenere Cipher.py
@@ -3,8 +3,7 @@ alphabet = "abcdefghijklmnopqrstuvwxyz"
 letter_index = dict(zip(alphabet, range(len(alphabet))))
 index_letter = dict(zip(range(len(alphabet)), alphabet))
 
-message = input("Enter your message: ")
-key = input("Enter the key: ")
+choice = input("Encrypt or Decrypt a message: ")
 
 def encrypt(message, key):
     #split into length of key
@@ -18,9 +17,36 @@ def encrypt(message, key):
         for letter in split:
             number = (letter_index[letter] + letter_index[key[i]]) % 26 #mod 26 = length of alphabet
             letter = index_letter[number]
-            i += 1
             encrypted_message += letter
+            i += 1
     
     return encrypted_message
 
-encrypt(message, key)
+def decrypt(encrypted, key):
+    split_message = [encrypted[i: i + len(key)] for i in range(0, len(encrypted), len (key))]
+    decrypted_message = ""
+    
+    for split in split_message:
+        i = 0
+        for letter in split:
+            number = (letter_index[letter] - letter_index[key[i]]) % 26 
+            letter = index_letter[number]
+            decrypted_message += letter
+            i += 1
+    
+    return decrypted_message
+
+
+def choose(choice):
+    choice = choice.lower()
+    key = input("Enter the key: ")
+    
+    if choice == "encrypt":
+        message = input("Enter your message: ")
+        print(encrypt(message, key))
+
+    else:
+        encrypted = input("Enter your encrypted message: ")
+        print(decrypt(encrypted, key))
+
+choose(choice)
